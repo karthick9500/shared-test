39cb8871840b352bb74f5057284838978a6cee2d
Karthick Veluswamy <karthick.veluswamy@publicissapient.com>
2019-03-04 22:50:37 +0000

    initial commit

--- vars/Oraclegetcall.groovy
+++ vars/Oraclegetcall.groovy
@@ -1,3 +1,20 @@
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.1')
+
+import groovyx.net.http.*
+import groovyx.net.http.ContentType.*
+import groovyx.net.http.Method.*
+import net.sf.json.*
+
+
+def oracleHttpObj
+def BasicAuth = 'Basic a2FydGhpY2s6YWRtaW4xMjM='
+
+oracleHttpObj = new HTTPBuilder('https://localhost:9002')
+oracleHttpObj.ignoreSSLIssues()
+
+
+
+
 def call()
 {
 
