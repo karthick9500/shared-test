456b176f4c952ce30e5ed6cca1a7f0023037fac1
Karthick Veluswamy <karthick.veluswamy@publicissapient.com>
2019-03-04 23:14:11 +0000

    initial commit

--- vars/Oraclegetcall.groovy
+++ vars/Oraclegetcall.groovy
@@ -14,6 +14,32 @@ import net.sf.json.*
 def call()
 {
 
-new HttpGet(script:this).run()
+  def oracleHttpObj
+  def BasicAuth = 'Basic a2FydGhpY2s6YWRtaW4xMjM='
+
+  oracleHttpObj = new HTTPBuilder('https://localhost:9002')
+  oracleHttpObj.ignoreSSLIssues()
+
+  oracleHttpObj.request(Method.GET, ContentType.JSON){ req ->
+    uri.path = "/management/weblogic/latest/edit/appDeployments"
+    headers.'Authorization' = "$BasicAuth"
+    //uri.query = [format:'json']
+    //body = jsonBody
+
+    respons.success = { resp, json ->
+        assert resp.status == 200
+        newItemId = json
+        println "$newItemId"
+    }
+    // not logged in response
+    response.'302' = { resp ->
+        throw new Exception("Stopping at item POST: uri: " + uri + "\n" +
+            "   You are not logging in properly. Item will not be created.")
+    }
+    response.failure = { resp, json ->
+      println "failure"
+      println json
+    }
+}
   println "printing from library"
 }
