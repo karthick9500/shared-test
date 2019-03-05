2327aa7a18dfdf53f22bb88e75d9bf40db41fc99
Karthick Veluswamy <karthick.veluswamy@publicissapient.com>
2019-03-05 00:50:31 +0000

    initial commit

--- vars/Oraclegetcall.groovy
+++ vars/Oraclegetcall.groovy
@@ -18,7 +18,7 @@ script{
   def patchOrg = """
   {"name" : "fairShare-10"}
   """
-  def response = httpRequest ignoreSslErrors: true, customHeaders: [[name: 'X-Requested-By', value: 'MyClient'],[name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: patchOrg, authentication :'oracle-call', url: "https://localhost:9002/management/weblogic/latest/edit/appDeployments"
+  def response = httpRequest ignoreSslErrors: true, customHeaders: [[name: 'Content-Type', value: 'application/json'],[name: 'Accept', value: 'application/json'],[name: 'X-Requested-By', value: 'MyClient']], httpMode: 'POST', requestBody: patchOrg, authentication :'oracle-call', url: "https://localhost:9002/management/weblogic/latest/edit/appDeployments"
 
 
   println(response.content)
