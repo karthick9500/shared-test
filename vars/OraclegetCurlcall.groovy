d334aade16fce41b673abe1f60063a165a8eebdf
Karthick Veluswamy <karthick.veluswamy@publicissapient.com>
2019-03-09 20:18:40 +0000

    tets

--- vars/OraclegetCurlcall.groovy
+++ vars/OraclegetCurlcall.groovy
@@ -1,6 +1,10 @@
 def call()
 {
 
+  def patchOrg = """
+  {"name": "deployer",
+  "sourcePath": "/deployments/fairShare.war"}
+  """
 
-  sh "curl -i X GET -H 'Authorization:Basic a2FydGhpY2s6YWRtaW4xMjM=' -H 'Content-Type:application/json'  -H 'X-Requested-By:MyClient' -H 'Accept:application/json' --insecure  'https://localhost:9002/management/weblogic/latest/edit/appDeployments'"
+  sh "curl -i X POST -d $patchOrg -H 'Authorization:Basic a2FydGhpY2s6YWRtaW4xMjM=' -H 'Content-Type:application/json'  -H 'X-Requested-By:MyClient' -H 'Accept:application/json' --insecure  'https://localhost:9002/management/weblogic/latest/edit/appDeployments'"
 }
