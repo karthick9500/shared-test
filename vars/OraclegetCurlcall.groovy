075c3eac3821ca0c679d048a523b448b61f3f0d1
Karthick Veluswamy <karthick.veluswamy@publicissapient.com>
2019-03-09 20:16:29 +0000

    tets

--- vars/OraclegetCurlcall.groovy
+++ vars/OraclegetCurlcall.groovy
@@ -2,5 +2,5 @@ def call()
 {
 
 
-  sh "curl -i -H 'Authorization : Basic a2FydGhpY2s6YWRtaW4xMjM=' --insecure X GET 'https://localhost:9002/management/weblogic/latest/edit/appDeployments'"
+  sh "curl -i -H 'Authorization : Basic a2FydGhpY2s6YWRtaW4xMjM=' -H 'Content-Type:application/json'  -H 'X-Requested-By:MyClient' -H 'Accept:application/json' --insecure X GET 'https://localhost:9002/management/weblogic/latest/edit/appDeployments'"
 }
